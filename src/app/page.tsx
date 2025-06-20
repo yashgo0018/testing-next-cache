155e18f54a432395bf61439b00a7179e789a189b
Yash Goyal <yashgo0018@gmail.com>
2025-06-21 00:18:59 +0200

    fixed a bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ const cachedUser = unstable_cache(async (userId: number) => {
 export default async function Home() {
   const user = await cachedUser(1);
 
+  console.log("user", user);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
